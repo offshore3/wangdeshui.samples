aade5799a5c6d6a85f9a9270d28bf8730aed6f0d
Jack Wang <wangds@shinetechchina.com>
2017-12-03 22:07:05 +0800

    xamarin test signalr core client

---
+++ Architecture/FullFrameWork/Shine.Samples/Shine.Samples.API/Hubs/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace Shine.Samples.API.Hubs
+{
+    public class ServerTimeHub : Hub
+    {
+        public string GetServerTime()
+        {
+            return DateTime.UtcNow.ToString();
+        }
+    }
+}
---
+++ Architecture/Xamarin/ASPNETCoreSignalRDemo/SharedStandardLib/HelloHub.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.Logging;
+
+namespace SharedStandardLib
+{
+    public class HelloHub
+    {
+        private HubConnection _hubConnection;
+        
+        public async void Start()
+        {
+            _hubConnection = await ConnectAsync("http://localhost:54358/hello");
+
+
+            _hubConnection.On<string>("Hello", data =>
+            {
+                Console.WriteLine(data);
+            });
+
+            _hubConnection.Closed += HubConnection_Closed;
+        }
+
+
+        public async void Close()
+        {
+           await  _hubConnection.DisposeAsync();
+        }
+
+        private static Task HubConnection_Closed(Exception arg)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static async Task<HubConnection> ConnectAsync(string baseUrl)
+        {
+            // Keep trying to until we can start
+            while (true)
+            {
+                var connection = new HubConnectionBuilder()
+                    .WithUrl(baseUrl)
+                    .WithConsoleLogger(LogLevel.Trace)
+                    .Build();
+                try
+                {
+                    await connection.StartAsync();
+                    return connection;
+                }
+                catch (Exception)
+                {
+                    await Task.Delay(1000);
+                }
+            }
+        }
+    }
+}
